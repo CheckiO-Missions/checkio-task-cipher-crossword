22b07b8fc67faed0b282b40b79f5fadeb2839014
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-08-27 14:54:28 +1200

    tests

--- verification/tests.py
+++ verification/tests.py
@@ -7,25 +7,66 @@ Each test is dict with
     "explanation" -- not necessary key, it's using for additional info in animation.
 """
 
-
 TESTS = {
     "Basics": [
         {
-            "input": [3, 2],
-            "answer": 5,
-            "explanation": "3+2=?"
+            "input": [
+                [
+                    [21, 6, 25, 25, 17],
+                    [14, 0, 6, 0, 2],
+                    [1, 11, 16, 1, 17],
+                    [11, 0, 16, 0, 5],
+                    [26, 3, 14, 20, 6]
+                ],
+                ['hello', 'habit', 'lemma', 'ozone', 'bimbo', 'trace']
+            ],
+            "answer": [
+                ['h', 'e', 'l', 'l', 'o'],
+                ['a', ' ', 'e', ' ', 'z'],
+                ['b', 'i', 'm', 'b', 'o'],
+                ['i', ' ', 'm', ' ', 'n'],
+                ['t', 'r', 'a', 'c', 'e'],
+            ],
         },
         {
-            "input": [5, 7],
-            "answer": 12,
-            "explanation": "5+7=?"
+            "input": [
+                [
+                    [19, 26, 8, 25, 18],
+                    [24, 0, 24, 0, 8],
+                    [4, 24, 23, 21, 3],
+                    [3, 0, 26, 0, 13],
+                    [8, 6, 15, 17, 13]
+                ],
+                ['world', 'tache', 'rings', 'water', 'racon', 'dress'],
+            ],
+            "answer": [
+                ['w', 'o', 'r', 'l', 'd'],
+                ['a', ' ', 'a', ' ', 'r'],
+                ['t', 'a', 'c', 'h', 'e'],
+                ['e', ' ', 'o', ' ', 's'],
+                ['r', 'i', 'n', 'g', 's'],
+            ]
         }
     ],
     "Extra": [
         {
-            "input": [6, 3],
-            "answer": 9,
-            "explanation": "6+3=?"
+            "input": [
+                [
+                    [19, 23, 22, 1, 23],
+                    [8, 0, 9, 0, 6],
+                    [10, 1, 6, 2, 22],
+                    [13, 0, 8, 0, 18],
+                    [22, 21, 18, 13, 22]
+                ],
+                ['users', 'crime', 'eagle', 'uncle', 'eking', 'siege']
+            ],
+            "answer": [
+                ['u', 's', 'e', 'r', 's'],
+                ['n', ' ', 'k', ' ', 'i'],
+                ['c', 'r', 'i', 'm', 'e'],
+                ['l', ' ', 'n', ' ', 'g'],
+                ['e', 'a', 'g', 'l', 'e'],
+            ]
         },
         {
             "input": [6, 7],
